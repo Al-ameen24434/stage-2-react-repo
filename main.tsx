49abe1a5e1cf510d05a4e18b58f2ce261707a576
al-ameen24434 <alameenabdulrahmon@gmail.com>
2025-10-28 16:10:48 +0100

    build fixes

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./client/src/App";
 import "./index.css";
-import { initializeTestData } from "./lib/initTestData";
+import { initializeTestData } from "./client/src/lib/initTestData";
 
 // Initialize test data on first load
 initializeTestData();
